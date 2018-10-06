98a28c100b241a72925f5e1f29253322ae144d38
alex2017mc <43913456+alex2017mc@users.noreply.github.com>
2018-10-06 20:16:18 +0300

    commit
    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let money = prompt("Ваш бюджет на месяц");
+let time = prompt("date: Введите дату, чч.мм.гггг");
+console.log(money),
+console.log(time);
+
+let appData = {
+    budget: "money",
+    timeData: "time",
+};
+let money2 = prompt("Введите обязательную статью расходов в этом месяце");
+let money3 = prompt("Во сколько обойдется?");
+let money2 = prompt("Введите обязательную статью расходов в этом месяце");
+let money3 = prompt("Во сколько обойдется?");
+appData.expenses[]
+
+
+alert ("money/30");
