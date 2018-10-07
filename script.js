c8d2b801b87a95fd05fc08f66c24309eea9691e8
alex2017mc <43913456+alex2017mc@users.noreply.github.com>
2018-10-07 12:46:55 +0300

    commit
    commit

--- script.js
+++ script.js
@@ -21,9 +21,48 @@ for ( let i = 0; i < 2; i++) {
         appData.expenses[a] = b;
         }
     else {
-        
-        }
-};
+    money = +prompt("Ваш бюджет на месяц",""),
+    time = prompt("date: Введите дату, YYYY-MM-DD","");
+        a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?","");
+        appData.expenses[a] = b;
+        } 
+} 
+/*через do и while
+do {
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+    b = prompt("Во сколько обойдется?","");
+    i++;
+if ((typeof(a))==='string' && (typeof(a)) !=null && (typeof(b)) != null 
+    && a !='' && b !=''&& a.length < 50 ) {
+    console.log("done");
+    appData.expenses[a] = b;
+} else {
+    money = +prompt("Ваш бюджет на месяц",""),
+    time = prompt("date: Введите дату, YYYY-MM-DD","");
+    a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+    b = prompt("Во сколько обойдется?","");
+    appData.expenses[a] = b;
+} while (1 < 2); 
+в while записываем условие */
+
+/* Через While
+While (1<2) {  
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?","");
+    i++;
+if ((typeof(a))==='string' && (typeof(a)) !=null && (typeof(b)) != null 
+    && a !='' && b !=''&& a.length < 50 ) {
+    console.log("done");
+    appData.expenses[a] = b;
+} else {
+    money = +prompt("Ваш бюджет на месяц",""),
+    time = prompt("date: Введите дату, YYYY-MM-DD","");
+    a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+    b = prompt("Во сколько обойдется?","");
+    appData.expenses[a] = b;
+}   
+} */
 appData.moneyPerDay = appData.budget / 30;
 
 alert ("Ежедневный бюджет: " + appData.moneyPerDay );
